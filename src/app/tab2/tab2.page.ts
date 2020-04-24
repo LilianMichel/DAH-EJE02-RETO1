2cfe204d70784f7a75ed8f9a420d7b29d605c976
LilianMichel <55470126+LilianMichel@users.noreply.github.com>
2020-04-24 10:21:17 -0600

    RETO1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { EstudianteService } from '../services/estudiante.service';
+import { Estudiante } from '../models/estudiante';
+
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +11,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public students: Estudiante[];
+
+  constructor(private service: EstudianteService) {
+    this.service.getStudents().subscribe(data => {
+      this.students = data.map ( e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data() as any
+        } as Estudiante;
+      });
+    });
+  }
 
 }
